a796704b4fa0eb9353140892077c30e8bb21b6b2
Deepanshu1026 <bishtdepanshu321@gmail.com>
2026-03-23 17:42:43 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [],
-  },
-  // Allow media from external CDNs (Pexels video)
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: blob: https:",
-              "media-src 'self' https://videos.pexels.com https://www.pexels.com",
-              "connect-src 'self'",
-            ].join("; "),
-          },
-        ],
-      },
-    ];
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https:; media-src 'self' https://videos.pexels.com https://www.pexels.com; connect-src 'self';"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
